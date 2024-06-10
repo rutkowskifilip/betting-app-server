8e9ddf8b464188870d9fc20576c5e72216d45232
rutkowskifilip <rutkowskifilip2005@gmail.com>
2024-06-10 14:29:34 +0200

    errors

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,35 @@
 const mysql = require("mysql");
 require("dotenv").config();
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL database:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
+let db;
+
+function handleDisconnect() {
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL database:", err);
+      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+    } else {
+      console.log("Connected to MySQL database");
+    }
+  });
+
+  db.on("error", (err) => {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("MySQL connection lost. Reconnecting...");
+      handleDisconnect(); // Reconnect if the connection is lost
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
 module.exports = db;
