34e0c6a52518c15f580c19c7e6a1d202c3864a70
rutkowskifilip <rutkowskifilip2005@gmail.com>
2024-06-10 16:24:04 +0200

    db error handling

--- utils/db.js
+++ utils/db.js
@@ -1,35 +1,77 @@
+// const mysql = require("mysql");
+// require("dotenv").config();
+
+// let db;
+
+// function handleDisconnect() {
+//   db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//   });
+
+//   db.connect((err) => {
+//     if (err) {
+//       console.error("Error connecting to MySQL database:", err);
+//       setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+//     } else {
+//       console.log("Connected to MySQL database");
+//     }
+//   });
+
+//   db.on("error", (err) => {
+//     if (err.code === "PROTOCOL_CONNECTION_LOST") {
+//       console.error("MySQL connection lost. Reconnecting...");
+//       handleDisconnect(); // Reconnect if the connection is lost
+//     } else {
+//       throw err;
+//     }
+//   });
+// }
+
+// handleDisconnect();
+
+// module.exports = db;
 const mysql = require("mysql");
 require("dotenv").config();
 
-let db;
+let pool;
 
-function handleDisconnect() {
-  db = mysql.createConnection({
+function createPool() {
+  pool = mysql.createPool({
+    connectionLimit: 10, // Dostosuj limit połączeń według potrzeb
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
   });
 
-  db.connect((err) => {
-    if (err) {
-      console.error("Error connecting to MySQL database:", err);
-      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
-    } else {
-      console.log("Connected to MySQL database");
-    }
-  });
-
-  db.on("error", (err) => {
+  pool.on("error", (err) => {
+    console.error("MySQL pool error:", err);
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      console.error("MySQL connection lost. Reconnecting...");
-      handleDisconnect(); // Reconnect if the connection is lost
+      // Przy utracie połączenia, ponownie tworzymy pulę połączeń
+      createPool();
     } else {
       throw err;
     }
   });
+
+  console.log("MySQL pool created");
 }
 
-handleDisconnect();
+createPool();
 
-module.exports = db;
+module.exports = {
+  query: (sql, values, callback) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        return callback(err);
+      }
+      connection.query(sql, values, (err, results) => {
+        connection.release(); // Zwolnij połączenie z puli
+        callback(err, results);
+      });
+    });
+  },
+};
